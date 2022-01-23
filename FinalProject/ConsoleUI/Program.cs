6464c3550e9103f7ce34eff227b5b4dc4e9710b7
Fırat Alçın <firatalcin04@gmail.com>
2022-01-23 21:06:04 +0300

    Update - Final Projesi - Backend

---
+++ FinalProject/ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Rehber
+    {
+    }
+}
