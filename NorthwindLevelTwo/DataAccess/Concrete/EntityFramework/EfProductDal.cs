4eff5a6baceefde87091ebfb79cc2bfe85c04864
Fırat Alçın <firatalcin04@gmail.com>
2022-03-19 22:23:05 +0300

    Update

---
+++ NorthwindLevelTwo/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using Core.DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepository<Product,NorthwindContext>, IProductDal
+    {
+       
+    }
+}
---
+++ NorthwindLevelTwo/MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebUI.Models;
+
+namespace MvcWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Listele()
+        {
+            var model = new ProductListViewModel()
+            {
+                Products = _productService.GetAll()
+            };
+
+            return View(model);
+        }
+    }
+}
