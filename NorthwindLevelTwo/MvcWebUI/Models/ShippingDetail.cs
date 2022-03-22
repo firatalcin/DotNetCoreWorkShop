243a20bfde1459bdc72436110c2f56c4ceca1c48
Fırat Alçın <firatalcin04@gmail.com>
2022-03-22 19:40:33 +0300

    Update

---
+++ NorthwindLevelTwo/MvcWebUI/Models/ShippingDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcWebUI.Models
+{
+    public class ShippingDetail
+    {
+       // [Required(ErrorMessage = "İsim gerekli")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //[Required]
+       // [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+       // [Range(18,65)]
+        public int Age { get; set; }
+    }
+}
