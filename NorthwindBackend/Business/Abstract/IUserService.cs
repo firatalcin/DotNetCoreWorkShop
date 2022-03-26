0c1a3634abcd164ff52b856bc2ccadb4772229a2
Fırat Alçın <firatalcin04@gmail.com>
2022-03-27 00:21:18 +0300

    Update

---
+++ NorthwindBackend/Business/Abstract/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        List<OperationClaim> GetClaims(User user);
+        void add(User user);
+        User GetByMail(string email);
+    }
+}
