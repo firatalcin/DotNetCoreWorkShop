0fbd76aaf15d763f6cff1c92fd4fb50212343bbb
Fırat Alçın <firatalcin04@gmail.com>
2022-04-01 19:40:27 +0300

    Update

---
+++ DotNetCore5Lessons/OrnekUygulama/OrnekUygulama/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekUygulama.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() //Controller'a gelen isteği karşılayan ve gerekli operasyonları gerçekleştiren metotlardır.
+        {
+            return View();
+        }
+    }
+}
---
+++ DotNetCore5Lessons/OrnekUygulama/OrnekUygulama/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekUygulama.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            //Veri üretildi...
+            return View(); // View fonksiyonu bu actiona ait view(cshtml) dosyasını tetikleyecek olan fonksiyondur.
+        }
+    }
+}
