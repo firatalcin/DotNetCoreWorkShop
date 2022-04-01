87469fe5fc5f13d763be87506b2e827ffc5850d6
Fırat Alçın <firatalcin04@gmail.com>
2022-04-01 18:50:42 +0300

    Update

---
+++ DotNetCore5Lessons/Degiskenler/DegiskenlerDers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DegiskenlerDers{
+
+    class Program
+    {
+        static void Main(string[] args){
+                     
+            //Değişkenler RAM'de Nasıl Tutulur ?
+            //int yas;
+            //string adi;
+            //string soyadi;
+
+            //snake_case PascalCase camelCase
+
+            // @static anlamlı keywordleri değişken olarak kullanmamızı sağlar
+
+            //
+
+
+            
+            
+
+            
+        }
+    }
+}
