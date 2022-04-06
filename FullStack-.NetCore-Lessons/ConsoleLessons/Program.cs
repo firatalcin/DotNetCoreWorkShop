97e6242720d8d4c1c5aae3a1115f4e76d03fcac9
Fırat Alçın <firatalcin04@gmail.com>
2022-04-06 18:27:00 +0300

    Update

---
+++ FullStack-.NetCore-Lessons/ConsoleLessons/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+// Dosya yolunu belirten yer
+namespace ConsoleLessons
+{
+    //Sınıf
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ders 1
+            //Yorum Satırları
+            //Console.WriteLine("Hello World!");
+            /*
+                Kodlar yukarıdan aşağıya doğru okunurlar 
+                Her satır noktalı vürgüllerle biter
+
+                //System kütüphanesi aracılığıyla yazılır.
+                Console.WriteLine("Merhaba Dünya");
+            */
+            #endregion
+
+            #region Ders 2
+            //Console.Write("Benim adım: ");
+            //Console.WriteLine("Fırat");
+            ////Console.Read(); // Enter tuşuna tıklayana kadar burada kalır
+            //Console.ReadKey(); // Herhangi bir tuşa basarak geçilebilir.
+            //Console.Write("soyadım: ");
+            //Console.WriteLine("Alçın"); 
+            #endregion
+
+            #region Ders 3
+            ////Console.ReadLine(); //Ekrana bir şey yazılırken bunu okur.
+            //Console.Write("İsminiz ne ? ");
+            //Console.ReadLine();            
+            //Console.Write("Soyadınız ne ? ");
+            //Console.ReadLine();          
+            //Console.WriteLine("İyi günler"); 
+            #endregion
+
+            #region Ders 4
+            //string name = "Fırat";
+            //name = "Rıfat";
+            //Console.WriteLine(name); 
+            #endregion
+
+            #region Ders 5
+            //Console.Write("İsminizi giriniz: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Soyadınızı Giriniz: ");
+            //string lastName = Console.ReadLine();
+
+            //Console.WriteLine("Hoş geldiniz " + name + " " + lastName); 
+            #endregion
+
+            #region Ders 6
+            //char first = 'a';
+            //byte test = 254;
+            //short test2 = 123;
+            //int number = 444;
+            //float test3 = 11.4F;
+            //double test4 = 11.4;
+            //decimal des = 15.8M;
+            //bool test5 = true; 
+            #endregion
+        }
+    }
+}
