655742d56e061de81302cceda3d1bbd7c21784dc
Fırat Alçın <firatalcin04@gmail.com>
2022-04-07 22:16:46 +0300

    Update

--- DotNetCore5Lessons/OrnekUygulama/OrnekUygulama/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrnekUygulama.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string ProductName { get; set; }
-        public int Quantity { get; set; }
-    }
-}
--- DotNetCore5Lessons/OrnekUygulama/OrnekUygulama/Models/ViewModels/UserProduct.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OrnekUygulama.Models.ViewModels
-{
-    public class UserProduct
-    {
-        public Product Product { get; set; }
-        public User User { get; set; }
-    }
-}
