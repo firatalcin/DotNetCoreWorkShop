a53889593deb6ebdc93668da690071f1d6b51fc0
Fırat Alçın <firatalcin04@gmail.com>
2022-04-09 00:53:44 +0300

    Update

---
+++ FullStackDotNetCore-Lessons/WinFormHomework/WinFormHomework/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormHomework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = Convert.ToInt32(textBox1.Text);
+               
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Lüften bir sayı giriniz.");
+                }
+                else if (number > 100)
+                {
+                    MessageBox.Show("Lütfen 100'den büyük bir sayı girmeyiniz."); 
+                    textBox1.Clear();
+                }
+                else
+                {
+                    number++;
+                    textBox1.Text = (number).ToString();
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lüften bir sayı giriniz.");
+                textBox1.Clear();
+            }
+        }
+    }
+}
+
