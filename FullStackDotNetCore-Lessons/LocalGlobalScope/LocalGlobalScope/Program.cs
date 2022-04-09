343b0c19185b5f99f4c7f9522229692a96b3fc1a
Fırat Alçın <firatalcin04@gmail.com>
2022-04-09 23:23:51 +0300

    Update

---
+++ FullStackDotNetCore-Lessons/LocalGlobalScope/LocalGlobalScope/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LocalGlobalScope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ders 1 - Local Alan
+            //try
+            //{
+            //    int age = 14;
+            //    Console.WriteLine(age);
+            //}
+            //catch (Exception ex)
+            //{
+            //age farklı scope'da tanımlandığı için field'a ulaşılamıyor.
+            //}
+            #endregion
+
+            #region Ders 2 - Global Alan
+            int age = 14;
+            try
+            {
+                Console.WriteLine(age);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(age); //global tanımlı olunca alt seviyede ki tüm scopelarda görünür.
+            }   
+            #endregion
+        }
+    }
+}
