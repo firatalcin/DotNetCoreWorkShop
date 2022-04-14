ba585ad45edd6ce6ba71c9fba16f5583f57e2a8e
Fırat Alçın <firatalcin04@gmail.com>
2022-04-15 02:47:18 +0300

    Update

---
+++ FullStackDotNetCore-Lessons/OOP/Constructor/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Product()
+        {
+            CreatedDate = DateTime.Now; 
+        }
+
+        public Product(string productName) : this()
+        {
+            ProductName = productName;           
+        }
+    }
+}
---
+++ FullStackDotNetCore-Lessons/OOP/Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Constructor.Models;
+using System;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ProductName = "Kumanda";
+
+            Product product2 = new Product("Kumanda");
+
+            Console.WriteLine($"Ürün adı: {product2.ProductName} - oluşturma tarihi: {product2.CreatedDate}");
+            
+
+        }
+    }
+}
