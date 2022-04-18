7fedb021b4be77dc6902a7b1849c823574973290
Fırat Alçın <firatalcin04@gmail.com>
2022-04-19 01:35:53 +0300

    Update

---
+++ FullStackDotNetCore-Lessons/HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller //controller
+    {
+        public string Index() //action
+        {
+            return "Uygulama çalışıyor";
+        }
+
+        public string Test()
+        {
+            return "bu test action'udur";
+        }
+
+        public IActionResult Selamla()
+        {
+            return View();
+        }
+
+        public IActionResult Selamla2()
+        {
+            return View("Test2");
+        }
+    }
+}
---
+++ FullStackDotNetCore-Lessons/HelloWorld/HelloWorld/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetUser()
+        {
+            User user = new User()
+            {
+                UserId = 1,
+                FirstName = "Fırat",
+                LastName = "Alçın"
+            };
+            return View();
+        }
+
+        [NonAction]
+        public string GetFullName(User user)
+        {
+            return user.FirstName + " " + user.LastName;
+        }
+    }
+}
