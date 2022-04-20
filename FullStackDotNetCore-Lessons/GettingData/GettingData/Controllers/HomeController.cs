24696d9fe9eb9680779899d0ea14d04baf542d83
Fırat Alçın <firatalcin04@gmail.com>
2022-04-21 01:38:40 +0300

    Update

---
+++ FullStackDotNetCore-Lessons/GettingData/GettingData/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GettingData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            return View();
+        }
+
+        // Home/UseQueryString?name=firat&surname=Alcin
+        public string UseQueryString(string name, string surName)
+        {
+            return "Uygulama Çalışıyor";
+        }
+    }
+}
---
+++ FullStackDotNetCore-Lessons/GettingData/GettingData/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GettingData.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product(){ProductId = 1, ProductName = "Televizyon" , CategoryName = "Teknoloji"},
+            new Product(){ProductId = 2, ProductName = "Telefon", CategoryName = "Teknoloji"},
+            new Product() {ProductId = 3, ProductName = "Tabak", CategoryName = "Mutfak"}
+        };
+
+        public IActionResult GetAllProducts()
+        {
+            return View(products);
+        }
+
+        public IActionResult GetProductById(int id)
+        {
+            var product = products.Find(x => x.ProductId == id);
+            if(product == null)
+            {
+                ViewBag.Error = "Verilen Id'de bir ürün bulunamadı";
+            }
+            return View(product);
+        }
+
+        public IActionResult GetProductByCategoryName(string categoryName)
+        {
+            var productList = products.FindAll(x => x.CategoryName == categoryName);
+            return View(productList);
+
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+            return View("GetAllProducts", products);
+        }
+    }
+}
