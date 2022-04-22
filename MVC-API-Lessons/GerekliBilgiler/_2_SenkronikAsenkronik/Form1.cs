b56999f3f3bcb591fc01f781cd014c9e13431161
Fırat Alçın <firatalcin04@gmail.com>
2022-04-22 19:52:21 +0300

    Update

---
+++ MVC-API-Lessons/GerekliBilgiler/_2_SenkronikAsenkronik/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_SenkronikAsenkronik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            Topla(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+        }
+
+        private async void btnCarp_Click(object sender, EventArgs e)
+        {
+           await CarpAsync(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+        }
+
+        private void Topla(int number1,int number2)
+        {
+            MessageBox.Show($"Toplam: {number1 + number2}");
+        }
+
+        private void Carp(int number1, int number2)
+        {
+            Thread.Sleep(10000);
+            MessageBox.Show($"Çarpım: {number1 * number2}");
+        }
+
+        private Task CarpAsync(int number1, int number2)
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+                MessageBox.Show($"Çarpım: {number1 * number2}");
+            });
+        }
+    }
+}
---
+++ MVC-API-Lessons/GerekliBilgiler/_3_ActionDelegeler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _3_ActionDelegeler
+{
+    public class Program
+    {
+        //func, action, predicate
+        static void Main(string[] args)
+        {
+            Action action = new Action(() =>
+            {
+                Console.WriteLine("Selam Fırat");
+            });
+
+            //Action<string> action2 = new Action<string>(SelamVer2);
+            //action2 += SelamVer3;
+            //action2.Invoke("Fırat");
+
+            Action<string> action2 = new Action<string>(name =>
+            {
+                Console.WriteLine("Selam " + name);
+            });
+
+            action2.Invoke("Fırat");
+        }
+
+        private static void SelamVer3(string name)
+        {
+            Console.WriteLine("Selam " + name + " Nasılsın ?");
+        }
+
+        private static void SelamVer2(string name)
+        {
+            Console.WriteLine("Selam " + name);
+        }
+
+        private static void SelamVer()
+        {
+            Console.WriteLine("Selam Fırat");
+        }
+    }
+}
