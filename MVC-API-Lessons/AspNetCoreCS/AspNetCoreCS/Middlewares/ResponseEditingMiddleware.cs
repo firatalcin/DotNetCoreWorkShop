c694e892df4ed09b79bfe8a536c4656470ba46cc
Fırat Alçın <firatalcin04@gmail.com>
2022-04-24 23:02:11 +0300

    Update

---
+++ MVC-API-Lessons/AspNetCoreCS/AspNetCoreCS/Middlewares/ResponseEditingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCS.Middlewares
+{
+    public class ResponseEditingMiddleware
+    {
+        RequestDelegate _requestDelegate;
+
+        public ResponseEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _requestDelegate.Invoke(context);
+            if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+                await context.Response.WriteAsync("Sayfa Bulunamadi");
+        }
+    }
+}
---
+++ MVC-API-Lessons/AspNetCoreCS/AspNetCoreCS/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreCS.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
