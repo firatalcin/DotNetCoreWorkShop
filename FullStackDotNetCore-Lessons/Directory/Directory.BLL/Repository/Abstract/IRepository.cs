3f9b9b057622dc3ebd37af2fde00d99e098ab723
Fırat Alçın <firatalcin04@gmail.com>
2022-05-04 17:42:17 +0300

    Update

---
+++ FullStackDotNetCore-Lessons/Directory/Directory.BLL/Repository/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Directory.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory.BLL.Repository.Abstract
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T,bool>> filter);
+        T GetByID(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ FullStackDotNetCore-Lessons/Directory/Directory.DAL/Context/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Directory.MAP.Configurations;
+using Directory.MODEL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory.DAL.Context
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-MVQ36J3\\SQLEXPRESS;database=MyDirectory;Trusted_connection=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PersonConfiguration());
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
