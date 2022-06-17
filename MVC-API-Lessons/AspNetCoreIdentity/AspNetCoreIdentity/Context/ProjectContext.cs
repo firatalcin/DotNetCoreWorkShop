732dcec1eaedd050529059fe27f6b34efeb5add5
Fırat Alçın <firatalcin04@gmail.com>
2022-06-17 20:32:13 +0300

    Update

---
+++ MVC-API-Lessons/AspNetCoreIdentity/AspNetCoreIdentity/Context/ProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCoreIdentity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Context
+{
+    public class ProjectContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+        }
+    }
+}
