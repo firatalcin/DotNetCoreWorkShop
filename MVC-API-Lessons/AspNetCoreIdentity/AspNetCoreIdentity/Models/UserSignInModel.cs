728bd73e9dc59355d964e2531672e472800ec240
Fırat Alçın <firatalcin04@gmail.com>
2022-06-22 16:07:59 +0300

    Update

---
+++ MVC-API-Lessons/AspNetCoreIdentity/AspNetCoreIdentity/Models/UserSignInModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.Models
+{
+    public class UserSignInModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adı gereklidir")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Parola gereklidir")]
+        public string Password { get; set; }
+    }
+}
