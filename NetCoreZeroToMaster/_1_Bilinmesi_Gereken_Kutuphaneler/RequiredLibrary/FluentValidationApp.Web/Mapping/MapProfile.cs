7ae7e9569fd2b889e4fcd7ef0a20250b9e818291
Fırat Alçın <firatalcin04@gmail.com>
2022-07-02 20:47:55 +0300

    AutoMapper Intro

---
+++ NetCoreZeroToMaster/_1_Bilinmesi_Gereken_Kutuphaneler/RequiredLibrary/FluentValidationApp.Web/Mapping/MapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FluentValidationApp.Web.DTOs;
+using FluentValidationApp.Web.Models;
+
+namespace FluentValidationApp.Web.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+        }
+    }
+}
