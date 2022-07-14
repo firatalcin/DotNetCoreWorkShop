670e4b57398b97b9bd8a47bb4d05708b47aca099
Fırat Alçın <firatalcin04@gmail.com>
2022-07-14 15:13:19 +0300

    Project1 Done

---
+++ OOP_Project/Proje_OOP/Proje_OOP/Context/ProjectDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proje_OOP.Entity;
+
+namespace Proje_OOP.Context
+{
+    public class ProjectDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-MVQ36J3\\SQLEXPRESS;database=OOPCoreDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ OOP_Project/Proje_OOP/Proje_OOP/Entity/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Proje_OOP.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
