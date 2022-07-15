9ad38409b08616c36dce9e2e602e01e04e506324
Fırat Alçın <firatalcin04@gmail.com>
2022-07-15 17:29:16 +0300

    NTier Arc and DataAccess

---
+++ OOP_Project/NTierOOPApp/DataAccessLayer/Concrete/Context/ProjectDb.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Context
+{
+    public class ProjectDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-MVQ36J3\\SQLEXPRESS;database=OOPCoreDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
