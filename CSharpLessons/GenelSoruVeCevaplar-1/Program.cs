391fb767a1926f2b035db3bc4091ba6c8f98c9e4
Fırat Alçın <firatalcin04@gmail.com>
2022-07-23 13:32:44 +0300

    namespace

---
+++ CSharpLessons/GenelSoruVeCevaplar-1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenelSoruVeCevaplar_1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+}
+
+// 1 - Namespace nedir : İçerisinde belirli sınıfları taşıyan gerektiğinde kodumuzda kullanacağımız yapılardır.
