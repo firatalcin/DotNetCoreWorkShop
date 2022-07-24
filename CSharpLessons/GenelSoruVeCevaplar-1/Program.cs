cedc140711f6be97a9303f57a3bf30c37ef4245a
Fırat Alçın <firatalcin04@gmail.com>
2022-07-24 17:41:37 +0300

    Strongly-Typed, Data Type Classification, Differences of Int and Int32

--- CSharpLessons/GenelSoruVeCevaplar-1/Program.cs
+++ CSharpLessons/GenelSoruVeCevaplar-1/Program.cs
@@ -4,9 +4,35 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba");
+            #region 1 - Namespace nedir : İçerisinde belirli sınıfları taşıyan gerektiğinde kodumuzda kullanarak yararlanacağımız yapılardır.
+
+            //namespace kavramı bizim projelerimizde dahil ettiğimiz 3. parti kütüphanelerdir.
+
+            #endregion 1 - Namespace nedir : İçerisinde belirli sınıfları taşıyan gerektiğinde kodumuzda kullanarak yararlanacağımız yapılardır.
+
+            #region 2 - Strongly-Typed : C#'ta farklı veri türleriyle matematiksel işlemler yapılamaz
+
+            string a = "5";
+            int b = 10;
+            //int topla = a + b; yapılamaz
+
+            #endregion 2 - Strongly-Typed : C#'ta farklı veri türleriyle matematiksel işlemler yapılamaz
+
+            #region 3 - Data Type sınıflandırılması : C#'ta iki tip veri sınıflandırması vardır. İlki gömülü olarak gelen tipler, ikincisi kullanıcının tanımladığı tipler
+
+            int c = 20; // gömülü olarak gelen
+
+            //class Ornek  = Kulanıcının tanımladığı
+
+            #endregion 3 - Data Type sınıflandırılması : C#'ta iki tip veri sınıflandırması vardır. İlki gömülü olarak gelen tipler, ikincisi kullanıcının tanımladığı tipler
+
+            #region 4 - Int ile Int32 arasında fark var mıdır : Herhangi bir fark yoktur. Int, Int32'nin kısaltılmış halidir.
+
+            Console.WriteLine(typeof(int));
+            Console.WriteLine(typeof(Int32));
+
+            #endregion 4 - Int ile Int32 arasında fark var mıdır : Herhangi bir fark yoktur. Int, Int32'nin kısaltılmış halidir.
+
         }
     }
 }
-
-// 1 - Namespace nedir : İçerisinde belirli sınıfları taşıyan gerektiğinde kodumuzda kullanarak yararlanacağımız yapılardır.
